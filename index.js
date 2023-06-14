55e4829525cd0b99dfe3601f335cc383da48a082
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-06-14 13:30:45 +0800

    Initially added JavaScript

---
+++ index.js
@@ -0,0 +1,14 @@
+const cardObjectDefinitions = [
+    {id: 1, imagePath:'/images/card-KingHearts.png'},
+    {id: 2, imagePath:'/images/card-JackClubs.png'},
+    {id: 3, imagePath:'/images/card-QueenDiamonds.png'},
+    {id: 4, imagePath:'/images/card-AceSpades.png'},
+];
+
+function createCard (cardItem) {
+    const cardElem = createElement('div');
+}
+
+function createElement (elemType) {
+    return document.createElement(elemType);
+}
