9e8af975b23394d9d2e21af8909a4798fa97cc80
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-06-16 11:57:32 +0800

    Added flip cards

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ function initializeNewGame () {
 function startRound () {
     initializeNewRound();
     collectCards();
+    flipCards(true);
 }
 
 function initializeNewRound () {
@@ -67,6 +68,24 @@ function addCardsToGridAreaCell (cellpositionClassName) {
 
     cards.forEach((card, index) => {
         addChildElement(cellPositionElem, card);
+    })
+}
+
+function flipCard (card, flipToBack) {
+    const innerCardElem = card.firstChild;
+
+    if (flipToBack && !innerCardElem.classList.contains('flip-it')) {
+        innerCardElem.classList.add('flip-it');
+    } else if (innerCardElem.classList.contains('flip-it')) {
+        innerCardElem.classList.remove('flip-it');
+    }
+}
+
+function flipCards (flipToBack) {
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            flipCard(card, flipToBack);
+        }, index * 100)
     })
 }
 
